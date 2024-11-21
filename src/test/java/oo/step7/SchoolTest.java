4bde1c6600c6ceed14f13dbc6d87dbf9c628cbcf
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-21 21:21:27 +0800

    test:add test should introduce when school has class and student call start

--- src/test/java/oo/step7/SchoolTest.java
+++ src/test/java/oo/step7/SchoolTest.java
@@ -1,27 +1,37 @@
 package oo.step7;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.io.PrintStream;
-//import java.util.List;
-//import oo.Klass;
-//import oo.School;
-//import oo.Student;
-//import oo.Teacher;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+import oo.Klass;
+import oo.School;
+import oo.Student;
+import oo.Teacher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SchoolTest {
 
-//    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-//
-//    @BeforeEach
-//    public void setup() {
-//        System.setOut(new PrintStream(outContent));
-//    }
-//
-//    private String systemOut() {
-//        return outContent.toString();
-//    }
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
+
+    @Test
+    public void should_introduce_when_school_has_class_and_student_call_start(){
+        Klass klass=new Klass(1);
+        Student bob = new Student(1, "Bob", 18);
+        bob.join(klass);
+        School school=new School(klass);
+        school.startSchool();
+        assertThat(systemOut()).contains("My name is Bob. I am 18 years old. I am in class 1.");
+    }
 }
