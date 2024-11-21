74efde666b50637ad9f53fb24089f141e12ed427
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-21 13:57:12 +0800

    feat:achieve step1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,16 +1,32 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private Integer id;
     private Integer age;
 
-    public Person(Integer id,String name,Integer age){
-        this.id=id;
-        this.name=name;
-        this.age=age;
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
     }
-    public String introduce(){
-    return String.format("My name is %s. I am %d years old.",name,age);
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return  Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, age);
     }
 }
