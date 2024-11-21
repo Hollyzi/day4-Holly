006e760927ffb3cd37892caca770f2d90cecf7d1
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-21 14:26:08 +0800

    feat:achieve step3

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,11 +1,22 @@
 package oo;
 
 public class Student extends Person{
+    private Klass klass;
     public Student(Integer id, String name, Integer age) {
         super(id, name, age);
     }
 
     public String introduce() {
-        return super.introduce()+ " I am a student.";
+        return super.introduce()+ " I am a student. I am in class "+klass.getNumber()+".";
     }
+
+    public boolean isIn(Klass klass){
+        return this.klass==klass;
+    };
+
+    public void join(Klass klass){
+        this.klass=klass;
+    };
+
+
 }
