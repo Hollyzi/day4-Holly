c9af321324ba6e3178b188bb7610e7c8c8774a01
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-21 13:40:20 +0800

    feat:achieve step1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,16 @@
 package oo;
 
 public class Person {
+    private String name;
+    private Integer id;
+    private Integer age;
+
+    public Person(Integer id,String name,Integer age){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+    public String introduce(){
+    return String.format("My name is %s. I am %d years old.",name,age);
+    }
 }
